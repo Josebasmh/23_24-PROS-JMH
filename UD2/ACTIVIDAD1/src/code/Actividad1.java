bd0b8ecfe2db9d7a79eb05b6c6453a4500f1b148
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-10-06 10:04:41 +0200

    ADD UD2-Actividad1
    Añadida la actividad 1 de la unidad 2 finalizada

---
+++ UD2/ACTIVIDAD1/src/code/Actividad1.java
@@ -0,0 +1,25 @@
+package code;
+
+public class Actividad1 extends Thread{
+
+	public void run() {
+		int sw=0;
+		for (int i=1;i<21;i++) {
+			switch (sw) {
+				case 0: {
+					System.out.println("Primero: "+i);
+					sw =1;
+					
+				}case 1:{
+					System.out.println("Segundo: "+i);
+					sw =0;
+				}
+			}	
+		}
+	}
+	
+	public static void main(String[] args) {
+		Actividad1 eje1 = new Actividad1();
+		eje1.start();	
+	}
+}
