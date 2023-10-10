14bcea34936e08c2472f071a081a8506f9572115
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-10-10 12:33:29 +0200

    UD2.ACT4_5.intento1.definitivo

--- UD2/ACTIVIDAD4_5/src/code/Actividad4.java
+++ UD2/ACTIVIDAD4_5/src/code/Actividad4.java
@@ -2,5 +2,27 @@ package code;
 
 public class Actividad4 extends Thread{
 
-	
+	int id;
+	String texto;
+	public Actividad4(int i) {
+		id=i;
+	}
+	@Override
+	public void run() {
+		if (id==1){
+			texto="PRIMERO ";
+		}else {
+			texto="SEGUNDO ";
+		}
+		for (int i=0;i<100;i++) {
+			System.out.println(texto + i);
+		}
+		System.out.println("Fin de Programa");
+	}
+	public static void main(String[] args) {
+		Actividad4 act1= new Actividad4(1);
+		Actividad4 act2= new Actividad4(2);
+		act1.start();
+		act2.start();
+	}
 }
--- UD2/ACTIVIDAD4_5/src/code/Actividad5.java
+++ UD2/ACTIVIDAD4_5/src/code/Actividad5.java
@@ -1,5 +1,34 @@
 package code;
 
-public class Actividad5 {
+public class Actividad5 extends Thread{
 
+	int id;
+	String texto;
+	public Actividad5(int i) {
+		id=i;
+	}
+	@Override
+	public void run() {
+		if (id==1){
+			texto="PRIMERO ";
+		}else {
+			texto="SEGUNDO ";
+		}
+		for (int i=0;i<100;i++) {
+			System.out.println(texto + i);
+			try {
+				if (id==1) {this.sleep(100);}else {this.sleep(200);}	
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Fin de Programa");
+	}
+	public static void main(String[] args) {
+		Actividad5 act1= new Actividad5(1);
+		Actividad5 act2= new Actividad5(2);
+		act1.start();
+		act2.start();
+	}
 }
