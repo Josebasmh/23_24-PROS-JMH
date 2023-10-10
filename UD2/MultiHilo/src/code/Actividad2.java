5de16a327c680457d16e2b5f4721405d29050c66
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-10-10 10:53:08 +0200

    COMPLETE actividad2 finalizada

--- UD2/MultiHilo/src/code/Actividad2.java
+++ UD2/MultiHilo/src/code/Actividad2.java
@@ -1,5 +1,22 @@
 package code;
 
-public class Actividad2 {
+public class Actividad2 extends Thread{
 
+	int n;
+	public Actividad2(int n) {
+		this.n=n;
+	}
+	@Override
+	public void run() {
+		for (int i=0;i<20;i++) {
+			System.out.println("Hilo " + n);
+		}
+	}
+	public static void main(String[] args) {
+		Actividad2 act21= new Actividad2(1);
+		Actividad2 act22= new Actividad2(2);
+		act21.start();
+		act22.start();
+		System.out.println("Fin del programa");
+	}
 }
