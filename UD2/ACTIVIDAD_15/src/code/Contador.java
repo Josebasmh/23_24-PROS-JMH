7e4dff8a7f26c042ac8f5cd29727446d0dbee890
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-10-29 14:59:58 +0100

    ADD-Actividad16

---
+++ UD2/ACTIVIDAD_15/src/code/Contador.java
@@ -0,0 +1,18 @@
+package code;
+
+class Contador {
+	private int c=0;
+	Contador (int c) { 
+		this.c = c; 
+	}
+	
+	public void incrementa() {
+		c=c+1;
+	}
+	public void decrementa() {
+		c=c-1;
+	}
+	public int getValor() {
+		return c; 
+	}
+}
