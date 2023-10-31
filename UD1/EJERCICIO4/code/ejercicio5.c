e9d37c332c348e0930996cb3ff54f34f042ed442
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-10-31 12:08:15 +0100

    UD1.ACT4.intento2.definitivo
    modificado el programa insertando un bucle.

--- UD1/EJERCICIO4/code/ejercicio5.c
+++ UD1/EJERCICIO4/code/ejercicio5.c
@@ -8,30 +8,16 @@
 void main (){
     pid_t pid;
     int status;
-
-    // Primer Hijo
-    if ((pid=fork())==-1){
-        exit(-1);
-    }else if (pid==0){
-        printf("Soy el hijo %d, mi padre es %d \n",getpid(),getppid());
-
-        // Segundo Hijo
-        if (pid=fork()==0){
+    int n=3; //Numero de procesos
+    
+    for (int i=0;i<n;i++){
+        if ((pid=fork()) == -1){
+            exit(-1);
+        }else if (pid==0){
             printf("Soy el hijo %d, mi padre es %d \n",getpid(),getppid());
-
-            // Tercer Hijo
-            if (pid=fork()==0){
-                printf("Soy el hijo %d, mi padre es %d \n",getpid(),getppid());
-            }else{
-                waitpid(pid,&status,0);    
-            }
-            
         }else{
             waitpid(pid,&status,0);
+            exit(0);
         }
-    }else{
-        waitpid(pid,&status,0);
-        printf("Soy el padre %d \n",getpid());
-    }
-    exit(0);
+    } 
 }
